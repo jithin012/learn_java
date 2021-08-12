d83efdddd36dd0485369ef60cbb8a6fe6292d632
jithin baby <jithinbaby@jithins-MBP.bbrouter>
2021-08-12 11:40:06 +0530

    static and default method in interface

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,47 @@
 package com.company;
 
+interface I {
+    default void show() {
+        System.out.println("I am interface 'i'");
+    }
+    static void demoStatic() {
+        System.out.println("Please don't call me via class instance. Since I am a static method on interface");
+    }
+}
+interface J {
+    default void show() {
+        System.out.println("I am interface 'j'");
+    }
+}
 
+class A {
+    public void show() {
+        System.out.println("I am class 'A'");
+    }
+}
+
+class C extends A implements I {
+    // TODO
+}
+
+class B implements I, J {
+    @Override
+    public void show() {
+        J.super.show();
+        //I.super.show();
+    }
+}
 public class Main {
 
     public static void main(String[] args) {
-        /*
-         * Prefer compile-time-error; Problem with type casting - JVM must test type casting at run-time
-         * Generics add stability to your code
-         * it makes most of the bugs and error to be detectable at compile time and not at run-time
-           reuse the same code with different input
-         * can avoid type casting
-         */
 
-//        Generic type = new Generic();
-//        type.demo();
-
-        GenericMethod method = new GenericMethod();
-        method.demo();
+        C c = new C();
+        c.show(); // "I am class 'A'" ie; Class has more power compare to interface called "Third Rule"
 
 
+        B b = new B();
+        b.show(); // we must define which default wants to call by @Override
+
+        I.demoStatic();
     }
 }
